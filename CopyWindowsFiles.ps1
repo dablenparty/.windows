6d013da52a8f391d46b009b7ced83f9418ae81d7
dablenparty <hdavenport8902@gmail.com>
2024-12-28 20:15:33 -0500

    Initial commit

---
+++ CopyWindowsFiles.ps1
@@ -0,0 +1,15 @@
+Write-Output "Copying PowerShell profile"
+Copy-Item -Path ".\PowerShell_profile.ps1" -Destination $PROFILE
+Write-Output "Copying Alacritty config"
+$AlacrittyConfigFolder = "$env:APPDATA\alacritty"
+if (-Not (Test-Path -PathType Container -Path "$AlacrittyConfigFolder")) {
+    New-Item -ItemType Directory -Path "$AlacrittyConfigFolder"
+}
+Copy-Item -Path ".\alacritty\alacritty.windows.toml" -Destination "$AlacrittyConfigFolder\alacritty.toml"
+Write-Output "Copying OMP config"
+$OmpConfig = ".\zen.omp.toml"
+Copy-Item -Path $OmpConfig -Destination "$env:POSH_THEMES_PATH\zen.omp.toml"
+Write-Output "Make sure to source $env:POSH_THEMES_PATH\zen.omp.toml as the OMP config in your PowerShell Profile"
+Write-Output "Copying neovim config"
+$NeovimConfigFolder = "$env:LOCALAPPDATA\nvim"
+Copy-Item -Path ".\nvim\.config\nvim" -Destination $NeovimConfigFolder -Recurse
